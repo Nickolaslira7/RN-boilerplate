acb8844eecb22fe590c552e550d9ec10c34031c2
Nickolas <nickolaslira.564@gmail.com>
2024-03-13 09:16:44 -0300

    feat: navigation drawer

--- src/routes/drawer.routes.jsx
+++ src/routes/drawer.routes.jsx
@@ -1,13 +1,17 @@
 import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import Home from "../screens/Home";
+import Category from "../screens/Category";
+import Profile from "../screens/Profile";
 
 const Drawer = createDrawerNavigator();
 
 const DrawerRoutes = () => {
   return (
-    <Drawer.Navigator screenOptions={{ headerShown: false }}>
+    <Drawer.Navigator screenOptions={{ headerShown: true }}>
       <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Category" component={Category} />
+      <Drawer.Screen name="Profile" component={Profile} />
     </Drawer.Navigator>
   );
 };
